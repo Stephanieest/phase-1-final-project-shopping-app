9a3030ce55f7612a5de3c01bb3fe8aad0a35c9a7
Stephanie <stephnjonjo@gmail.com>
2024-10-16 14:00:02 +0300

    adds main padding and grid

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ fetch('db.json')
     })
     .catch(error => console.error('Error fethching products'))
 
-
+function renderHome (){
+    mainContent.innerHTML = '<h2>Welcome to our shopping app, your one-stop destination for trendy and delightful products that cater to all your needs! Explore a curated collection featuring everything from cozy apparel to stylish accessories, perfect for elevating your lifestyle.Whether you’re looking to spruce up your wardrobe, find the perfect gift, or add a touch of charm to your home, our diverse selection has something for everyone. Shop now and discover unique items that express your style and creativity!</h2>'
+}
 
 function renderProducts(filteredProducts){
 // resets the content which removes products that were displayed before
@@ -36,5 +38,4 @@ function renderProducts(filteredProducts){
         mainContent.appendChild(productDiv);
     })
 }
-
 
