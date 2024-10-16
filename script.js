131bb699b699617496fecc5cc0407e52c919be53
Stephanie <stephnjonjo@gmail.com>
2024-10-16 10:21:26 +0300

    fetch db.json

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const mainContent = document.getElementById('main-content');
 const searchInput = document.getElementById('search');
-let products = []
-let cart = []
+let products = [];
+let cart = [];
+
+
+fetch('db.json')
+    .then(response => response.json())
+    .then(data => {
+        products = data;
+        renderProducts(products);
+    })
 
 
 
