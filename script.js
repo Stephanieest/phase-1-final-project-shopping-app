60141901f0fbed999d917d3cab5f24f188600573
Stephanie <stephnjonjo@gmail.com>
2024-10-16 11:28:49 +0300

    adds new codes that resets content, created a div element, img tag, h3 tag, two p tags and a button and appends the productDiv

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const searchInput = document.getElementById('search');
 let products = [];
 let cart = [];
 
-
+// fetch products from db.json
 fetch('db.json')
     .then(response => response.json())
     .then(data => {
@@ -13,4 +13,25 @@ fetch('db.json')
 
 
 
-
+function renderProducts(filteredProducts){
+// resets the content which removes products that were displayed before
+    mainContent.innerHTML=''
+    filteredProduct.forEach(product => {
+// creates a div element and gives it a className
+        const productDiv = document.createElement('div')
+        productDiv.className = 'product'
+// creates an img tag - displays the product image
+// creates a h3 tage - for the product name
+// creates two p tags - one for the product description and the other for the product price
+// creates a button that when its clicked, it calls the addToCart func with the products ID
+        productDiv.innerHTML = `
+        <img src = "${product.image}">
+        <h3>${product.name}</h3>
+        <p>${product.description}</p>
+        <p>Price: ${product.price}</p>
+        <button onclick="addToCart(${product.id})">Add to Cart</button>
+        `;
+// appends the productDiv
+        mainContent.appendChild(productDiv);
+    })
+}
