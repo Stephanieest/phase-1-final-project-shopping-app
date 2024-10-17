c5ea882b719a5536369476fcfe4e9d2d75976f74
Stephanie <stephnjonjo@gmail.com>
2024-10-17 09:22:32 +0300

    adds an event listener and preventDefault for the home

--- script.js
+++ script.js
@@ -14,8 +14,14 @@ fetch('db.json')
 
     // ADDS HOME CONTENT BUT CAN'T DISPLAY IT YET
 function renderHome (){
-    mainContent.innerHTML = '<h2>Welcome to our shopping app, your one-stop destination for trendy and delightful products that cater to all your needs! Explore a curated collection featuring everything from cozy apparel to stylish accessories, perfect for elevating your lifestyle.Whether you’re looking to spruce up your wardrobe, find the perfect gift, or add a touch of charm to your home, our diverse selection has something for everyone. Shop now and discover unique items that express your style and creativity!</h2>'
+    mainContent.innerHTML = '<h2>Welcome to my shopping app, your one-stop destination for trendy and delightful products that cater to all your needs! Explore a curated collection featuring everything from cozy apparel to stylish accessories, perfect for elevating your lifestyle.Whether you’re looking to spruce up your wardrobe, find the perfect gift, or add a touch of charm to your home, our diverse selection has something for everyone. Shop now and discover unique items that express your style and creativity!</h2>'
 }
+
+document.getElementById('home').addEventListener('click', (e) => {
+    e.preventDefault();
+    renderHome();
+});
+
 
 function renderProducts(filteredProducts){
 // resets the content which removes products that were displayed before
@@ -33,7 +39,7 @@ function renderProducts(filteredProducts){
         <h3>${product.name}</h3>
         <p>${product.description}</p>
         <p>Price: ${product.price}</p>
-        <button onclick="addToCart(${product.id})">Add to Cart</button>
+        <button onclick="addToCart(${product.id})">Add to Cart 🛒</button>
         `;
 // appends the productDiv
         mainContent.appendChild(productDiv);
@@ -55,3 +61,5 @@ function removeFromCart(productId){
     alert("Item has been removed from your cart!❌")
     renderCart()
 }
+
+
